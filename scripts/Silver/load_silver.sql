e5dad98964306ab879a66b441e3049525781efcc
Anirudhyadavv <anirudhyadav162000@gmail.com>
2026-01-25 23:39:45 +0530

    Update load_silver.sql

--- scripts/Silver/load_silver.sql
+++ scripts/Silver/load_silver.sql
@@ -25,7 +25,7 @@ cst_create_date
 FROM(
 SELECT *, ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC	) as flag_last
 FROM dw_bronze.crm_cust_info
-WHERE cst_id IS NOT NULL) t
+WHERE cst_id IS NOT NULL AND cst_id != 0) t
 WHERE flag_last = 1;
 
 --------------------------------------------------------------------------------------
