628dfc0a5e8b779f5b61455920eeae70137bd394
Anirudhyadavv <anirudhyadav162000@gmail.com>
2026-02-24 22:35:42 +0530

    Update 01_sales_trend_overtime_analysis.sql

--- scripts/analytics/01_sales_trend_overtime_analysis.sql
+++ scripts/analytics/01_sales_trend_overtime_analysis.sql
@@ -4,34 +4,48 @@ Change Over Time Analysis
 ===============================================================================
 Purpose:
     - Analyze how sales, customers, and quantities evolve over time
-    - Identify growth trends and seasonal patterns
+    - Identify growth trends and potential seasonality
     - Support time-based business performance evaluation
 
+Grain:
+    - Monthly and Yearly
+
 SQL Functions Used:
-    - Date Functions: DATE_FORMAT(), YEAR()
-    - Aggregate Functions: SUM(), COUNT()
+    - DATE_FORMAT(), YEAR()
+    - SUM(), COUNT()
 ===============================================================================
 */
 
--- Monthly analysis of sales, customers, and quantities
+
+-- ============================================================================
+-- 1. MONTHLY CHANGE OVER TIME ANALYSIS
+-- ============================================================================
+-- Business Question:
+-- How do sales, customer activity, and quantities change month by month?
+
 SELECT
     DATE_FORMAT(order_date, '%Y-%m') AS order_month,
-    SUM(sales_amount) AS total_sales,
-    COUNT(DISTINCT customer_key) AS total_customers,
-    SUM(quantity) AS total_quantity
+    SUM(sales_amount)                AS total_sales,
+    COUNT(DISTINCT customer_key)     AS total_customers,
+    SUM(quantity)                    AS total_quantity
 FROM dw_gold.fact_sales
 WHERE order_date IS NOT NULL
-GROUP BY order_month
+GROUP BY DATE_FORMAT(order_date, '%Y-%m')
 ORDER BY order_month;
 
 
--- Yearly analysis of sales, customers, and quantities
+-- ============================================================================
+-- 2. YEARLY CHANGE OVER TIME ANALYSIS
+-- ============================================================================
+-- Business Question:
+-- How does overall business performance evolve year by  year?
+
 SELECT
-    YEAR(order_date) AS order_year,
-    SUM(sales_amount) AS total_sales,
-    COUNT(DISTINCT customer_key) AS total_customers,
-    SUM(quantity) AS total_quantity
+    YEAR(order_date)                 AS order_year,
+    SUM(sales_amount)                AS total_sales,
+    COUNT(DISTINCT customer_key)     AS total_customers,
+    SUM(quantity)                    AS total_quantity
 FROM dw_gold.fact_sales
 WHERE order_date IS NOT NULL
-GROUP BY order_year
+GROUP BY YEAR(order_date)
 ORDER BY order_year;
